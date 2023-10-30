e24ca5e1fe74639f501fe3229aefcec438f9a006
Spriow <xilli.martin@gmail.com>
2023-10-31 00:09:39 +0100

    Tailwind working with SvelteKit
    ```bash
    npm install
    ```
    
    ```bash
    npm run dev
    ```
    
    ```bash
    npm run build
    ```
    
    ```bash
    npm run deploy
    ```
    
    Got some problems with less saas and postcss. Btw
    this is working with postcss.

--- care-bear/src/routes/+page.svelte
+++ care-bear/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="postcss">
+	:global(html) {
+		font-family: sans-serif;
+		font-size: 16px;
+		line-height: 1.5;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+</style>
