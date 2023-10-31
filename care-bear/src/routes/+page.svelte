9038545e67f1f002fc1b4a530e64ae0d7b496a24
spirow73 <67395826+spirow73@users.noreply.github.com>
2023-10-31 12:03:11 +0100

    Trying to trigger actions

--- care-bear/src/routes/+page.svelte
+++ care-bear/src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<h1 class="text-3xl font-bold underline">Hello!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <style lang="postcss">
